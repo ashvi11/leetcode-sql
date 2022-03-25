d9ef4d3f605618b3cd3b554182ab5998e6b161b7
Ashvi Soni <74107153+ashvi11@users.noreply.github.com>
2022-03-25 17:51:29 -0400

    Update sql-1677-products-worth-over-invoices.sql

--- sql-1677-products-worth-over-invoices.sql
+++ sql-1677-products-worth-over-invoices.sql
@@ -5,5 +5,5 @@ group by 1
 order by 1
 
 
-## used left join because we wanted products even if they are not on invoices- this will make their value null in the restult, 
-so used ifnull() to convert it to 0
+# used left join because we wanted products even if they are not on invoices- this will make their value null in the restult, 
+# so used ifnull() to convert it to 0
