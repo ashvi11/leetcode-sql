ddef76d8f3c2bcfb2268e424965f546acbbe5c54
Ashvi Soni <74107153+ashvi11@users.noreply.github.com>
2022-03-26 16:43:12 -0400

    Update sql-185-department-top-three-salaries.sql

--- sql-185-department-top-three-salaries.sql
+++ sql-185-department-top-three-salaries.sql
@@ -1,8 +1,7 @@
-
-select d.name as Department, temp.name as Employee, temp.Salary from 
-Department d,
-(select id, name, salary, departmentId, RANK() over (partition by departmentId order by salary desc) as 'rank' from Employee) temp 
-where d.id = temp.departmentId and temp.rank in (1, 2, 3);
+select d.name as Department, temp.name as Employee, temp.Salary 
+from Department d,
+(select id, name, salary, departmentId, DENSE_RANK() over (partition by departmentId order by salary desc) as 'rank' from Employee) temp 
+where d.id = temp.departmentId and temp.rank in (1, 2, 3)
 
 # Here, I have partitioned rank by department, that is, I want only 1 ranked salary from each department. So I created a 'temp' table which is same as
 # Employee table but with rank. And instead of joining Employee table, I joined this table with a condition rank = 1, 2, 3
