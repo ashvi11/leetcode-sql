9079bdf6390cb1085666211a9e8d19db124f4e8c
Ashvi Soni <74107153+ashvi11@users.noreply.github.com>
2022-03-26 19:54:36 -0400

    Update sql-1327-list-the-products-ordered-in-a-period.sql

--- sql-1327-list-the-products-ordered-in-a-period.sql
+++ sql-1327-list-the-products-ordered-in-a-period.sql
@@ -2,3 +2,5 @@ select p.product_name, sum(o.unit) as unit from Products p, Orders o
 where p.product_id = o.product_id and o.order_date between '2020-02-01' and '2020-02-29'
 group by 1
 having sum(o.unit) >= 100
+
+# can also use where o.order_date like '2020-02%' instead of between- this also works
