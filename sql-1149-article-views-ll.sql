44b8fb593190658f7e696058146ccf515ffd78e7
Ashvi Soni <74107153+ashvi11@users.noreply.github.com>
2022-03-27 18:17:57 -0400

    Update sql-1149-article-views-ll.sql

--- sql-1149-article-views-ll.sql
+++ sql-1149-article-views-ll.sql
@@ -4,3 +4,4 @@ from res
 where article_count > 1 
 order by 1
 
+# created another table- res which has count of people who viewd distict articles on the same date- then selected those ids which had count > 2
