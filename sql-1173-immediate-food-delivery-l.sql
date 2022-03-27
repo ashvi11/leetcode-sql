1e1a039545794f6bf238c5aa26fa0baabc81abdb
Ashvi Soni <74107153+ashvi11@users.noreply.github.com>
2022-03-27 19:53:13 -0400

    Create sql-1173-immediate-food-delivery-l.sql

---
+++ sql-1173-immediate-food-delivery-l.sql
@@ -0,0 +1,12 @@
+select round(sum(count_imm)/count(delivery_id)*100,2) as immediate_percentage 
+from 
+(select delivery_id,
+case when 
+order_date = customer_pref_delivery_date then 1 
+else 0
+end
+as count_imm
+from Delivery) temp
+
+# created a temp table which stores 1 if delivery is immediate, else 0. Sum of 1's would be total number of immediate deliveries. 
+# So, we sum that and count total number of rows in the table since delivery_id is the primary key. Then division
