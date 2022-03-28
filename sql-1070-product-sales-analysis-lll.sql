0cc9a7b18fed28013aa67a68636b79f17bfca776
Ashvi Soni <74107153+ashvi11@users.noreply.github.com>
2022-03-27 21:03:31 -0400

    Update sql-1070-product-sales-analysis-lll.sql

--- sql-1070-product-sales-analysis-lll.sql
+++ sql-1070-product-sales-analysis-lll.sql
@@ -1,5 +1,5 @@
 
 select product_id,`year`as first_year,quantity,price
-from(select *, rank()over(partition by product_id order by year asc) as 'rank1'
+from(select *, rank()over(partition by product_id order by year) as 'rank1'
 from Sales) temp
 where rank1 = 1
