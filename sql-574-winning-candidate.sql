d615ab0dc9a0d762dece38231d9f9afe8d05509b
Ashvi Soni <74107153+ashvi11@users.noreply.github.com>
2022-04-10 14:15:56 -0400

    Update sql-574-winning-candidate.sql

--- sql-574-winning-candidate.sql
+++ sql-574-winning-candidate.sql
@@ -1,4 +1,12 @@
 #Approach 1
+select name 
+from Candidate c, Vote v
+where c.id = v.candidateId
+group by v.candidateId
+order by count(v.id) desc
+limit 1
+
+#Approach 2- CTE
 
 with res as
 (select c.name , count(v.id) as total from Candidate c, Vote v 
@@ -7,7 +15,7 @@ group by v.candidateId)
 
 select name from res order by total desc limit 1
 
-#Approach 2- WINDOW FUNCTION
+#Approach 3- CTE + WINDOW FUNCTION
 
 with res as 
 (select c.name, rank() over (order by count(v.id) desc) as rnk from Candidate c, Vote v 
