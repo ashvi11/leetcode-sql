c12129921f8e1cfaaf12311f427bb4edc2d9c2a3
Ashvi Soni <74107153+ashvi11@users.noreply.github.com>
2022-04-14 17:10:31 -0400

    Create sql-1393-capital-gain-loss.sql

---
+++ sql-1393-capital-gain-loss.sql
@@ -0,0 +1,26 @@
+
+Approach 1-
+select stock_name, sum(price * case when operation = 'Buy' then -1 else 1 end) as capital_gain_loss 
+from Stocks 
+group by 1
+
+#here, multiply the price with -1 if it is buy, else 1 and calculate the sum
+
+
+Approach 2-
+select stock_name, 
+sum(case when operation = 'Sell' then price else null end) - sum(case when operation = 'Buy' then price else null end) as capital_gain_loss 
+from Stocks 
+group by 1
+
+#here, sum when operations = buy - sum when operations = sell
+
+#Approach 2- solved myself
+with res as (SELECT sell.stock_name, sell.total_sell, buy.total_buy FROM 
+(select stock_name, sum(price) as total_sell from Stocks where operation = 'Sell' group by stock_name) sell, 
+(select stock_name, sum(price) as total_buy from Stocks where operation = 'Buy' group by stock_name)buy
+where sell.stock_name = buy.stock_name)
+
+select stock_name, total_sell-total_buy as capital_gain_loss from res
+
+# here, I made two columns- total buy for each stock and total sell for each stock, the subtracted it in the query
