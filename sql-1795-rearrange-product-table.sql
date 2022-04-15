09e95997c134991cf06b195115084c53d6c9c860
Ashvi Soni <74107153+ashvi11@users.noreply.github.com>
2022-04-15 11:09:17 -0400

    Update sql-1795-rearrange-product-table.sql

--- sql-1795-rearrange-product-table.sql
+++ sql-1795-rearrange-product-table.sql
@@ -5,4 +5,3 @@ select product_id, 'store2' as 'store', store2 as price from Products
 union
 select product_id, 'store3' as 'store', store3 as price from Products) temp
 where price is not null
-order by 1
