182c597bb4173b8642abe8d64cbde004fdcfe606
Ashvi Soni <74107153+ashvi11@users.noreply.github.com>
2022-04-17 14:29:50 -0400

    Update sql-1581-customer-who-visited-but-did-not-make-any-transactions.sql

--- sql-1581-customer-who-visited-but-did-not-make-any-transactions.sql
+++ sql-1581-customer-who-visited-but-did-not-make-any-transactions.sql
@@ -1,7 +1,16 @@
 
+#Approach 1- solved myself
 select customer_id, count(visit_id) as count_no_trans
 from (select v.visit_id, v.customer_id, t.transaction_id
       from  Visits v left join  Transactions t
       using(visit_id)) temp
+where transaction_id is null
+group by 1
+
+
+#Approach 2
+select v.customer_id, count(visit_id) as count_no_trans
+from  Visits v left join  Transactions t
+using(visit_id)
 where transaction_id is null
 group by 1
