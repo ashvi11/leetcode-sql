5b7d8b1aa4d66799b4fb9aaa840ec70c0b0656f0
Ashvi Soni <74107153+ashvi11@users.noreply.github.com>
2022-04-20 12:44:29 -0400

    Update sql-570-managers-with-atleast-5-direct-reports.sql

--- sql-570-managers-with-atleast-5-direct-reports.sql
+++ sql-570-managers-with-atleast-5-direct-reports.sql
@@ -1,5 +1,11 @@
+#Approach 1- row_number()- self
 with res as
 (select id, name, managerId, row_number() over(partition by managerId) as rn
 from Employee)
 
 select name from Employee where id in (select distinct managerId from res where rn >= 5)
+
+#Approach 2- having- leetcode discuss
+select name 
+from Employee 
+where id in (select managerId from Employee group by 1 having count(distinct id) >= 5)
