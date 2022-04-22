8f69d1d982bd01f74f6c77a3826e7bc32735e041
Ashvi Soni <74107153+ashvi11@users.noreply.github.com>
2022-04-22 13:50:07 -0400

    Update sql-2004-the-number-of-seniors-and-juniors-to-join-the-company.sql

--- sql-2004-the-number-of-seniors-and-juniors-to-join-the-company.sql
+++ sql-2004-the-number-of-seniors-and-juniors-to-join-the-company.sql
@@ -1,11 +1,41 @@
 
-
 # Approach 1-
+# shorter version of approach 2. For more detailed information, see approach 2
+# make a table with partition between junior and senior
+# make a table hired_senior which will hire(count) all seniors with running salary <= 70,000
+# make a table hired_junior which will hire(count) all juniors with running salary <= remaining_budget
+# union both these table
+
+with running_sal as (
+    select employee_id, experience, 
+    sum(salary) over(partition by experience order by salary rows between unbounded preceding and current row) as running_salary
+    from Candidates
+    order by experience, running_salary),
+    
+hired_seniors as (
+    select 'Senior' as experience, count(*) as accepted_candidates
+    from running_sal
+    where running_salary <= 70000 and experience = 'Senior'),
+
+hired_juniors as (
+    select 'Junior' as experience, count(*) as accepted_candidates
+    from running_sal
+    where running_salary <= 70000-(select ifnull(max(running_salary),0) from running_sal where running_salary <= 70000 and experience = 'Senior') and experience = 'Junior')
+
+select * from hired_seniors
+union
+select * from hired_juniors
+
+
+
+
+
+# Approach 2-
 # make a table senior and a table junior with running salaries- 2 tables here
 # make a table hired_senior which will hire(count) all seniors with running salary <= 70,000- 1 table
 # make a table remaining_salary which will calculate how much budget is remaining afte hiring the last person from senio table- 1 table
 # make a table hired_junior which will hire(count) all juniors with running salary <= remaining_budget- 1 table
-# join both count tables
+# union both count tables
 
 with seniors as
   (select employee_id, salary, sum(salary) over(order by salary rows between unbounded preceding and current row) as running_salary
