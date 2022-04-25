c3e8c3294cc6025c16fab7c20936fee2c5b2dfe3
Ashvi Soni <74107153+ashvi11@users.noreply.github.com>
2022-04-25 11:41:08 -0400

    Update sql-1112-highest-grade-for-each-student.sql

--- sql-1112-highest-grade-for-each-student.sql
+++ sql-1112-highest-grade-for-each-student.sql
@@ -1,3 +1,15 @@
+#Approach 1-
+with res as
+    (select student_id, course_id, grade, dense_rank() over(partition by student_id order by        grade desc, course_id) as rnk
+    from Enrollments)
+    
+select student_id, course_id, grade 
+from res 
+where rnk = 1
+order by 1
+
+
+# Approach 2-
 select student_id, min(course_id) as course_id, grade from
 (select *, RANK() over (partition by student_id order by grade desc) as 'rank' from Enrollments) temp 
 where temp.rank = 1 
