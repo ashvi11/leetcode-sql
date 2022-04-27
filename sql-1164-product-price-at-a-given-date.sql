9878b7ae699a16b1f3830566b832e822f8020bce
Ashvi Soni <74107153+ashvi11@users.noreply.github.com>
2022-04-27 09:51:49 -0400

    Update sql-1164-product-price-at-a-given-date.sql

--- sql-1164-product-price-at-a-given-date.sql
+++ sql-1164-product-price-at-a-given-date.sql
@@ -1,4 +1,16 @@
- with res as
+# Approach 1
+
+with res as (select *, rank() over(partition by product_id order by change_date desc) as rnk from Products
+where datediff('2019-08-16', change_date) >= 0)
+
+select distinct p.product_id, ifnull(r.new_price, 10) as price from Products p left join res r 
+on p.product_id = r.product_id and rnk = 1
+
+
+
+#Approach 2
+
+with res as
     (select product_id, max(change_date) as most_recent
     from Products
     where datediff('2019-08-16', change_date) >= 0
