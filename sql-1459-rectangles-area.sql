98bcfa6a30d410f0ec6cef23f9bb267fa466b207
Ashvi Soni <74107153+ashvi11@users.noreply.github.com>
2022-05-16 11:41:40 -0400

    Create sql-1459-rectangles-area.sql

---
+++ sql-1459-rectangles-area.sql
@@ -0,0 +1,5 @@
+select distinct p1.id as p1, p2.id as p2, abs(p1.x_value-p2.x_value)*abs(p1.y_value-p2.y_value) as area
+from Points p1, Points p2
+where p1.id < p2.id
+  and abs(p1.x_value-p2.x_value)*abs(p1.y_value-p2.y_value) != 0
+order by 3 desc, 1, 2
